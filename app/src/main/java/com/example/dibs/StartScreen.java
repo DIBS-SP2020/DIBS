256c5d37a56733e3fdce828b1bcd872a27a8eb1f
Herman Li <herman_hmli@yahoo.com>
2020-02-29 14:41:51 -1000

    Initial commit

---
+++ app/src/main/java/com/example/dibs/StartScreen.java
@@ -0,0 +1,14 @@
+package com.example.dibs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+    }
+}
