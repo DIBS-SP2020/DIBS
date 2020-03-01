94676e94f55fdb8963820d587f01d616c2bee895
Herman Li <herman_hmli@yahoo.com>
2020-02-29 16:07:25 -1000

    Added some back code for password check through server with volley

--- app/src/main/java/com/example/dibs/StartScreen.java
+++ app/src/main/java/com/example/dibs/StartScreen.java
@@ -5,36 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 public class StartScreen extends AppCompatActivity {
-    private EditText Name;
-    private EditText Password;
-    private Button Login;
+    RequestQueue requestQueue = Volley.newRequestQueue(this);
+    String url = getText(R.string.server_url).toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
-
-        Name = (EditText)findViewById(R.id.name);
-        Password = (EditText)findViewById(R.id.password);
-        Login = (Button)findViewById(R.id.login);
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    validate(Name.getText().toString(), Password.getText().toString());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 
-    public void validate (View view) throws InterruptedException {
-        if ((view.findViewById().equals("Admin")) && (userPassword.equals("Admin"))) {
+    public void login(View view) {
+        EditText usernameTextField = (EditText) findViewById(R.id.name);
+        EditText passwordTextField = (EditText) findViewById(R.id.password);
+        String username = usernameTextField.getText().toString();
+        String password = passwordTextField.getText().toString();
+
+        //JsonObjectRequest request = new JsonObjectRequest();
+        if(username.equals("Admin") && password.equals("Admin")) {
             Intent intent = new Intent(StartScreen.this, Home.class);
             startActivity(intent);
         }
